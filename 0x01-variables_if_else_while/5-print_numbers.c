909840642d82d4ee4e6921ea55bbfca867fe091f
Your Name <akram.essalhi@gmail.com>
2023-10-16 18:39:29 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,13 +3,13 @@
 /**
  * main - Prints numbers from 0 to 9.
  *
- * Return: Always 0 (Success)
+ * Return 0 
  */
 int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <=9; i++)
 	{
 		printf("%d", i);
 	}
