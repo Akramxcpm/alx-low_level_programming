6aa7c757bdcd431e449d1984d262fe78239ad165
Your Name <akram.essalhi@gmail.com>
2023-10-16 17:47:58 -0700

    herewegoagian

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+int main(void){
 
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
+char min ='a';
+
+while(min <= 'z')
+{
+	putchar(min);
+	min++;
 }
 
+return 0;
+}
